8813e7e46bb1e71ef8322e4c647752dd93ecd1dc
thomashaile <gthomashaile@gmail.com>
2020-05-20 01:25:26 +0200

    solved

--- isolate/refactored-exercise-fs-error-middleware/api/routes.js
+++ isolate/refactored-exercise-fs-error-middleware/api/routes.js
@@ -2,19 +2,12 @@ const handlers = require('./handlers');
 const express = require('express');
 
 // create the router
-_;
-
-router.use((req, res, next) => {
-  console.log('routes!');
-  next();
-});
-
-router.get('/', (req, res) => {
-  res.send('routes!');
-});
+//_;
+const router = express.Router();
 
 // write the routes!
-
-
+router.get('/', handlers.home);
+router.get('/list', handlers.list);
+router.post('/create/:name', handlers.create);
 // export the router
-_;
+module.exports = router;
