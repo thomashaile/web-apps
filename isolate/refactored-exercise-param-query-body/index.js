9a7a62f8467134ca47a5c2e27062d760247dc5d1
thomashaile <gthomashaile@gmail.com>
2020-05-21 00:53:32 +0200

    solved

--- isolate/refactored-exercise-param-query-body/index.js
+++ isolate/refactored-exercise-param-query-body/index.js
@@ -13,6 +13,7 @@ const morgan = require('morgan');
 const cors = require('cors');
 
 const config = require('./config');
+const api = require('./api/routes');
 
 const app = express();
 
@@ -21,60 +22,27 @@ app.use(bodyParser.json());
 
 // log to file
 app.use(morgan('combined', {
-  stream: fs.createWriteStream(
-    path.join(__dirname, 'access.log'),
-    { flags: 'a' }
-  )
+    stream: fs.createWriteStream(
+        path.join(__dirname, 'access.log'), { flags: 'a' }
+    )
 }));
 // log to console
 app.use(morgan('dev'));
 
 app.use('/', express.static(path.join(__dirname, 'client')));
+app.use('/api', api);
 
 app.get('/api', (req, res) => {
-  res.send('api!');
+    res.send('api!');
 });
 
-app.post('/api/param/:value', (req, res) => {
-  const paramValue = req.params.value;
-
-  console.log(`param value: ${paramValue}`);
-
-  const responseData = {
-    paramValue,
-  };
-  res.json(responseData);
-});
-
-app.post('/api/query', (req, res) => {
-  const queryValue = req.query.value
-
-  console.log(`query value: ${queryValue}`);
-
-  const responseData = {
-    queryValue,
-  };
-  res.json(responseData);
-});
-
-app.post('/api/body', (req, res) => {
-  const bodyValue = req.body.value;
-
-  console.log(`body value: ${bodyValue}`);
-
-  const responseData = {
-    bodyValue,
-  };
-  res.json(responseData);
-});
-
-app.use(function (err, req, res, next) {
-  console.error(err.stack);
-  res.status(500).end();
+app.use(function(err, req, res, next) {
+    console.error(err.stack);
+    res.status(500).end();
 });
 
 app.listen(config.PORT, () => {
-  console.log(
-    `listening at http://localhost:${config.PORT} (${config.MODE} mode)`
-  );
+    console.log(
+        `listening at http://localhost:${config.PORT} (${config.MODE} mode)`
+    );
 });
