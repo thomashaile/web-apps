2a73645fa4927e65dfe5522e400bd17d5827d55e
thomashaile <gthomashaile@gmail.com>
2020-05-25 20:55:11 +0200

    schema-validation-solved

--- isolate/input-validation-exercise-handlers/api/handlers.js
+++ isolate/input-validation-exercise-handlers/api/handlers.js
@@ -10,182 +10,184 @@ const readFile = util.promisify(fs.readFile)
 const writeFile = util.promisify(fs.writeFile)
 
 const handlers = {
-  create: async (req, res) => {
+    create: async(req, res) => {
 
-    const newFurniture = req.body
+        const newFurniture = req.body
 
-    try {
-      const fruitDataString = await readFile(DATA_PATH, 'utf-8');
-      const fruitData = JSON.parse(fruitDataString);
+        try {
+            const fruitDataString = await readFile(DATA_PATH, 'utf-8');
+            const fruitData = JSON.parse(fruitDataString);
 
-      newFurniture.id = fruitData.nextId;
-      fruitData.nextId++;
+            newFurniture.id = fruitData.nextId;
+            fruitData.nextId++;
 
-      const isValid = _;
+            const isValid = tv4.validate(newFurniture, FRUIT_SCHEMA);
 
-      if (_) {
-        const error = _;
-        console.error(error)
+            if (!isValid) {
+                const error = tv4.error;
+                console.error(error)
 
-        res.status(400).json({
-          error: {
-            message: error.message,
-            dataPath: error.dataPath
-          }
-        })
-        return
-      }
+                res.status(400).json({
+                    error: {
+                        message: error.message,
+                        dataPath: error.dataPath
+                    }
+                })
+                return
+            }
 
-      fruitData.fruit.push(newFurniture);
+            fruitData.fruit.push(newFurniture);
 
-      const newFurnitureDataString = JSON.stringify(fruitData, null, '  ');
+            const newFurnitureDataString = JSON.stringify(fruitData, null, '  ');
 
-      await writeFile(DATA_PATH, newFurnitureDataString);
+            await writeFile(DATA_PATH, newFurnitureDataString);
 
-      res.json(newFurniture);
+            res.json(newFurniture);
 
-    } catch (err) {
-      console.log(err);
+        } catch (err) {
+            console.log(err);
 
-      if (err && err.code === 'ENOENT') {
-        res.status(404).end();
-        return;
-      }
+            if (err && err.code === 'ENOENT') {
+                res.status(404).end();
+                return;
+            }
 
-      next(err);
-    }
-
-  },
-  readAll: async (req, res) => {
-    try {
-      const fruitDataString = await _;
-      const fruitData = _;
-
-      res.json(fruitData.fruit);
-
-    } catch (err) {
-      console.log(err)
-
-      if (err && err.code === 'ENOENT') {
-        res.status(404).end();
-        return;
-      }
-
-      next(err);
-    }
-  },
-  readOne: async (req, res) => {
-    const idToUpdateStr = _;
-    const idToUpdate = _;
-
-    try {
-      const fruitDataString = await readFile(DATA_PATH, 'utf-8');
-      const fruitData = JSON.parse(fruitDataString);
-      const selectedFurniture = fruitData.fruit
-        .find(profile => profile.id === idToUpdate);
-
-      res.json(selectedFurniture);
-
-    } catch (err) {
-      console.log(err)
-
-      if (err && err.code === 'ENOENT') {
-        res.status(404).end();
-        return;
-      }
-
-      next(err);
-    }
-  },
-  update: async (req, res) => {
-    const idToUpdateStr = req.params.id;
-    const idToUpdate = Number(idToUpdateStr);
-
-    const newFurniture = req.body
-    newFurniture.id = idToUpdate;
-    const isValid = _
-
-    if (!isValid) {
-      const error = _
-      console.error(error)
-
-      res.status(400).json({
-        error: {
-          _,
-          _
+            next(err);
         }
-      })
-      return
-    }
 
-    try {
-      const fruitDataString = await readFile(DATA_PATH, 'utf-8');
-      const fruitData = JSON.parse(fruitDataString);
+    },
+    readAll: async(req, res) => {
 
-      const entryToUpdate = fruitData.fruit
-        .find(profile => profile.id === idToUpdate);
+        try {
+            const fruitDataString = await readFile(DATA_PATH, "utf-8");
+            const fruitData = JSON.parse(fruitDataString);
 
-      if (entryToUpdate) {
-        const indexOfFurniture = fruitData.fruit
-          .indexOf(entryToUpdate);
-        fruitData.fruit[indexOfFurniture] = newFurniture;
+            res.json(fruitData.fruit);
+            console.log(DATA_PATH);
 
-        const newFurnitureDataString = JSON.stringify(fruitData, null, '  ');
+        } catch (err) {
+            console.log(err)
 
-        await writeFile(DATA_PATH, newFurnitureDataString);
+            if (err && err.code === 'ENOENT') {
+                res.status(404).end();
+                return;
+            }
 
-        res.json(newFurniture);
-      } else {
-        res.json(`no entry with id ${idToUpdate}`);
-      }
+            next(err);
+        }
+    },
+    readOne: async(req, res) => {
+        const idToUpdateStr = req.params.id;
+        const idToUpdate = Number(idToUpdateStr);
 
-    } catch (err) {
-      console.log(err);
+        try {
+            const fruitDataString = await readFile(DATA_PATH, 'utf-8');
+            const fruitData = JSON.parse(fruitDataString);
+            const selectedFurniture = fruitData.fruit
+                .find(profile => profile.id === idToUpdate);
 
-      if (err && err.code === 'ENOENT') {
-        res.status(404).end();
-        return;
-      }
+            res.json(selectedFurniture);
 
-      next(err);
-    }
-  },
-  delete: async (req, res) => {
-    const idToDeleteStr = req.params.id;
-    const idToDelete = Number(idToDeleteStr);
+        } catch (err) {
+            console.log(err)
 
-    try {
-      const fruitDataString = await readFile(DATA_PATH, 'utf-8');
-      const fruitData = JSON.parse(fruitDataString);
+            if (err && err.code === 'ENOENT') {
+                res.status(404).end();
+                return;
+            }
 
-      const entryToDelete = fruitData.fruit
-        .find(profile => profile.id === idToDelete);
+            next(err);
+        }
+    },
+    update: async(req, res) => {
+        const idToUpdateStr = req.params.id;
+        const idToUpdate = Number(idToUpdateStr);
 
-      if (entryToDelete) {
+        const newFurniture = req.body
+        newFurniture.id = idToUpdate;
+        const isValid = tv4.validate(newFurniture, FRUIT_SCHEMA)
 
-        fruitData.fruit = fruitData.fruit
-          .filter(profile => profile.id !== entryToDelete.id);
+        if (!isValid) {
+            const error = tv4.error
+            console.error(error)
 
-        const newFurnitureDataString = JSON.stringify(fruitData, null, '  ');
+            res.status(400).json({
+                error: {
+                    message: error.message,
+                    dataPath: error.dataPath
+                }
+            })
+            return
+        }
 
-        await _;
+        try {
+            const fruitDataString = await readFile(DATA_PATH, 'utf-8');
+            const fruitData = JSON.parse(fruitDataString);
 
-        res.json(entryToDelete);
-      } else {
-        res.json(`no entry with id ${idToUpdate}`);
-      }
+            const entryToUpdate = fruitData.fruit
+                .find(profile => profile.id === idToUpdate);
 
-    } catch (err) {
-      console.log(err);
+            if (entryToUpdate) {
+                const indexOfFurniture = fruitData.fruit
+                    .indexOf(entryToUpdate);
+                fruitData.fruit[indexOfFurniture] = newFurniture;
 
-      if (err && err.code === 'ENOENT') {
-        res.status(404).end();
-        return;
-      }
+                const newFurnitureDataString = JSON.stringify(fruitData, null, '  ');
 
-      next(err);
-    }
-  },
+                await writeFile(DATA_PATH, newFurnitureDataString);
+
+                res.json(newFurniture);
+            } else {
+                res.json(`no entry with id ${idToUpdate}`);
+            }
+
+        } catch (err) {
+            console.log(err);
+
+            if (err && err.code === 'ENOENT') {
+                res.status(404).end();
+                return;
+            }
+
+            next(err);
+        }
+    },
+    delete: async(req, res) => {
+        const idToDeleteStr = req.params.id;
+        const idToDelete = Number(idToDeleteStr);
+
+        try {
+            const fruitDataString = await readFile(DATA_PATH, 'utf-8');
+            const fruitData = JSON.parse(fruitDataString);
+
+            const entryToDelete = fruitData.fruit
+                .find(profile => profile.id === idToDelete);
+
+            if (entryToDelete) {
+
+                fruitData.fruit = fruitData.fruit
+                    .filter(profile => profile.id !== entryToDelete.id);
+
+                const newFurnitureDataString = JSON.stringify(fruitData, null, '  ');
+
+                await writeFile(DATA_PATH, newFurnitureDataString);
+
+                res.json(entryToDelete);
+            } else {
+                res.json(`no entry with id ${idToUpdate}`);
+            }
+
+        } catch (err) {
+            console.log(err);
+
+            if (err && err.code === 'ENOENT') {
+                res.status(404).end();
+                return;
+            }
+
+            next(err);
+        }
+    },
 };
 
 module.exports = handlers;
